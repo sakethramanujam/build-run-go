c1ccd77da53155767bc0db96654d9c40b532c8db
sakethramanujam <ramanujamsamavedam@hotmail.com>
2019-07-11 15:00:47 +0530

    add square function

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def recursive_fib(n):
 def index(n):
     return str(recursive_fib(n))
 
+@app.route('/square/<int:n>')
+def square(n):
+       return str(n**2)
+
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0',port=8080)
