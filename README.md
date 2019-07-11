c33cf04267dbe4fb0f5ab660690caf2f64a07519
sakethramanujam <ramanujamsamavedam@hotmail.com>
2019-07-11 12:23:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build Run Go
+
+- [x] Testing Google Cloud Build and Google Cloud Run for serverless architecture and hassle-free deployment.
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, request
+
+
+app = Flask(__name__)
+
+def recursive_fib(n):
+    n = int(n)
+    if (n <= 1):  
+       return 1  
+    else:  
+       return (recursive_fib(n-1) + recursive_fib(n-2))  
+
+@app.route('/fib/<int:n>')
+def index(n):
+    return str(recursive_fib(n))
+
+if __name__=='__main__':
+    app.run(debug=True)
